a06a7ee9320bec024fa84553b4974f85151ed7ee
willgarbo <willianggbatista@gmail.com>
2024-09-20 00:23:11 -0300

    Adding readme description; creation of collection and documents; diagram improvement

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Recommendation system database model
+
+Modelagem e implementação de um banco de dados orientado a documentos (mongodb) para um sistema de recomendação fictício.
+
+## O que tem neste repo?
+ - Tem um drawio com uma arquitetura geral do fluxo de recomendação e o a modelagem conceitual, lógica e física do sistema de recomendação;
+ - Tem um docker-compose para subir um mongodb com a implementação do modelo físico citado;
+
+## Subindo o banco de dados
+
+> Nota: Para subir o banco de dados é necessário ter o Docker e Docker 
+> Compose instalados e configurados.
+
+Executar o comando: `docker compose up -d` para subir os container
--- create-schema.js
+++ create-schema.js
@@ -1,63 +1,65 @@
-db = db.getSiblingDB('rec_system_db');
+db = db.getSiblingDB("rec_system_db");
 
-db.createCollection('user_profile');
-db.createCollection('product');
-db.createCollection('product_category');
-db.createCollection('recommendation');
-db.createCollection('interaction');
+db.createCollection("user_profile");
+db.createCollection("product");
+db.createCollection("product_category");
+db.createCollection("recommendation");
+db.createCollection("interaction");
 
 db.user_profile.insertMany([
  {
-    name: 'Will',
-    email: 'will@will.ru',
-    preferences: ["horror", "top_10_books", "fiction movies", "cars"]
+    name: "Will",
+    email: "will@will.ru",
+    preferences: ["horror", "top_10_books", "fiction movies", "cars", "most_selled"]
   },
   {
-    name: 'Naty',
-    email: 'naty@naty.ch'
+    name: "Naty",
+    email: "naty@naty.ch"
   },
   {
-    name: 'Joana',
-    email: 'joana@joana.jp',
+    name: "Joana",
+    email: "joana@joana.jp",
     bornDate: Date(),
     preferences: ["japonese things", "top_10_novels"]
   } 
 ]);
 
-db.createCollection("interaction", {
-   validator: {
-      $jsonSchema: {
-         bsonType: "object",
-         required: [ "type", "userId", "product" ],
-         properties: {
-            type: {
-               bsonType: "string",
-               description: "must be a string and is required"
-            },
-            userId: {
-               bsonType: "int",
-               description: "must be an integer in [ 2017, 3017 ] and is required"
-            },
-            product: {
-               bsonType: "object",
-               required: [ "name" ],
-               properties: {
-                  name: {
-                     bsonType: "string",
-                     description: "must be a string if the field exists"
-                  },
-                  product_category: {
-                     bsonType: "object",
-                     properties: {
-                        name: {
-                     bsonType: "string",
-                     description: "must be a string if the field exists"
-                        },
-                     }
-                  }
-               }
-            }
-         }
-      }
-   }
-})
+
+db.product_category.insertMany([
+  {
+      name: "Livros",
+      tags: ["top_100"]
+  },
+  {
+      name: "Eletrônicos",
+      tags: ["most_selled"]
+  },
+  {
+      name: "Beleza",
+  },
+  {
+      name: "Roupas",
+      tags: ["top_2", "most_selled"]
+  }
+]);
+
+const bookCategoryId = db.product_category.findOne({name: "Livros"}, {_id: 1})["_id"]
+const cloutingId = db.product_category.findOne({name: "Roupas"}, {_id: 1})["_id"]
+const lookingId = db.product_category.findOne({name: "Beleza"}, {_id: 1})["_id"]
+db.product.insertMany([
+  {
+      name: "Como fazer amigos e influenciar pessoas",
+      tags: ["top_100"],
+      productCategoryId: bookCategoryId
+  },
+  {
+      name: "Camiseta Malloy Brickleberry",
+      tags: ["never_selled"],
+      productCategoryId: cloutingId
+  },
+  {
+      name: "Gel anti-espinhas",
+      tags: ["top_10_teenager", "most_selled", "teen"],
+      productCategoryId: lookingId
+  }
+]);
